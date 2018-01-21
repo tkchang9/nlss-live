cf06210026a09861cb8440325992a166710086a5
Tyler Chang <tylerkchang@gmail.com>
2018-01-21 14:34:26 -0500

    postcss changes

--- dist/bundle.js
+++ dist/bundle.js
@@ -57,7 +57,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
+/******/ 	__webpack_require__.p = "dist/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 0);
@@ -70,7 +70,15 @@
 "use strict";
 
 
+__webpack_require__(1);
+
 console.log('This is working.');
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: ModuleBuildError: Module build failed: Syntax Error \n\n(13:6) Undefined variable $mainRed\n\n \u001b[90m 11 | \u001b[39m \u001b[33m}\u001b[39m\n \u001b[90m 12 | \u001b[39m \u001b[33m.titleLive\u001b[39m \u001b[33m{\u001b[39m\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 13 | \u001b[39m     color\u001b[33m:\u001b[39m $mainRed\u001b[33m;\u001b[39m\n \u001b[90m    | \u001b[39m     \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 14 | \u001b[39m \u001b[33m}\u001b[39m\n \u001b[90m 15 | \u001b[39m\n\n    at runLoaders (/home/tyler/webDev/nlssLive/node_modules/webpack/lib/NormalModule.js:195:19)\n    at /home/tyler/webDev/nlssLive/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/tyler/webDev/nlssLive/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at context.callback (/home/tyler/webDev/nlssLive/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Promise.resolve.then.then.catch (/home/tyler/webDev/nlssLive/node_modules/postcss-loader/lib/index.js:189:44)\n    at <anonymous>");
 
 /***/ })
 /******/ ]);
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,38 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: 'dist/'
     },
     module: {
         rules: [
             {
-                test: /\.pcss$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            importLoaders: 1,
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader'
-                    }
-                ]
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader','postcss-loader']
+                })
             },
             {
                 test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
+                exclude: /node_modules/,
                 use: {
                   loader: 'babel-loader',
                   options: {
                     presets: ['env']
-                  }
+                    }
                 }
-              }
+            }
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: "styles.css",
+            allChunks: true
+        })
+    ]
 };
