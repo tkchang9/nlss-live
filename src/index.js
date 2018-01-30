99079fb16ef7408157a04dfdb2775a1207c40b20
tkchang9 <tylerkchang@gmail.com>
2018-01-29 19:56:51 -0500

    js twitch api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
+console.log('This is working.');
+
 require("./ampurpose.scss");
 
 require("./main.scss");
 
-console.log('This is working.')
+import { ajaxGet } from "./js/ajaxget.js";
+
+const nlssMembers = ["northernlion", "rockleesmile", "jsmithoti", 
+"cobaltstreak", "alpacapatrol", "last_grey_wolf", "baertaffy", 
+"roundtablepodcast"]
+const apiStream = "https://api.twitch.tv/kraken/streams/";
+const apiChannel = "https://api.twitch.tv/kraken/channels/";
+
+function streamInfo(channel) {
+    let streamData, offlineData, streamOnline, streamGame, streamName, streamURL;
+    streamData = ajaxGet(apiStream + channel);
+    switch (streamData.stream) {
+        case null: //stream is offline
+            streamOnline = false;
+            streamGame = 'Offline';
+            offlineData = ajaxGet(apiChannel + channel);
+            streamName = offlineData.display_name;
+            streamURL = offlineData.url;
+            break;
+        case undefined: //stream closed
+            streamOnline = false;
+            streamGame = 'Account closed';
+            break;
+        default: //stream online
+            streamOnline = true;
+            streamGame = streamData.stream.game;
+            streamName = streamData.channel.display_name;
+            streamURL = streamData.channel.url;
+            break;
+    }
+    console.log(streamOnline, streamGame, streamName, streamURL);
+}
+
+streamInfo('northernlion');
---
+++ src/js/ajaxget.js
@@ -0,0 +1,21 @@
+export default { ajaxGet }
+
+function ajaxGet(url) {
+    return new Promise(function(resolve, reject) {
+        let req = new XMLHttpRequest();
+        req.open("GET", url);
+        req.onload = function() {
+            if (req.status === 200) {
+                resolve(req.response);
+            } else {
+                reject(new Error(req.statusText));
+            }
+        };
+ 
+        req.onerror = function() {
+            reject(new Error("Network error"));
+        };
+ 
+        req.send();
+    });
+}
